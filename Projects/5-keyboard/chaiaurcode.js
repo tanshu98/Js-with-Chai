ddf0d22b0b1c8e3c60700df7bbffb8559679b1ea
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-01-18 15:36:50 +0530

    Added KeyCode Project

--- Projects/5-keyboard/chaiaurcode.js
+++ Projects/5-keyboard/chaiaurcode.js
@@ -1,1 +1,25 @@
-console.log('Project 5');
+const insert = document.querySelector('#insert');
+
+window.addEventListener('keydown',(e)=> {
+    console.log(e);
+    insert.innerHTML = `
+    
+    <table>
+  <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' '? 'Space': e.key }</td>
+    <td>${e.keyCode}</td>
+    <td>${ e.code }</td>
+  </tr>
+</table>
+    
+    
+    
+    `
+})
+
+// 
